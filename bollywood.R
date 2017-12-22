f8459d09bab67c9f5eab4b5a991a1816fe77ed7b
Abhishek Banerjee <abhishekbanerjee99@gmail.com>
2017-12-22 10:20:05 +0530

    Add files via upload

---
+++ bollywood.R
@@ -0,0 +1,87 @@
+ 
+#	Import the Bollywood data set in Rstudio in a variable named bollywood
+
+
+  bollywood<-read.csv("bollywood.csv",stringsAsFactors = F)
+  View(bollywood)
+  #reading csv file woth strings as factors set to false by default
+
+#	When you import a data set, R stores character vectors as factors (by default)
+# You can check the structure of the data frame by using str()
+  str(bollywood)
+
+# You can change the attribute 'Movie' from factor to character type using the given command
+  bollywood$Movie <- as.character(bollywood$Movie)
+	 
+
+#Q1.
+#	Access the last 10 movies (from the bottom of the Bollywood data frame) using column bollywood$Movie
+# Store the names of those movies in last_10 vector (in the same order)
+     
+	last_10 <- tail(bollywood,10)
+	#using tail to get last 10 rows of the data frame
+#Q2.
+#	Find out the total number of  missing values (NA) in the bollywood data frame.
+# Store the result in na_bollywood vector
+	
+	na_bollywood <-  sum(is.na(bollywood))
+	#finding out the total number of NA's in bollywood data frame
+	
+#Q3
+#	Write the command to find out which movie tops the list in terms of Total Collections
+# Store the movie name in variable named top_movie
+	
+	top_movie<-bollywood$Movie[bollywood$Tcollection==max(bollywood$Tcollection,na.rm=TRUE)]
+  #using max function to find max Tcollection
+  
+#Q4
+#	Write the command to find out which movie comes second on the list in terms of Total Collections
+# Store the movie name in variable named top_2_movie
+	
+	top_2_movie<-bollywood$Movie[bollywood$Tcollection==(sort(bollywood$Tcollection,decreasing = T)[2])]
+	#sorting the data frame in descending order and selecting the second element
+	
+# Now let's find out the movies shot by Shahrukh, Akshay and Amitabh separately.
+# subset() function is used for that. The code has already been written for you. 
+	
+	shahrukh <- subset(bollywood, Lead == "Shahrukh")
+	akshay <- subset(bollywood, Lead == "Akshay")
+	amitabh <- subset(bollywood, Lead  == "Amitabh")
+
+# You can view what the above data frames look like
+  View(shahrukh)
+  View(akshay)
+  View(amitabh)
+#Q5
+#	What is the total collection of Shahrukh, Akshay and Amitabh movies individually?
+# You can use	a column named 'Tcollection' for this 
+ 
+  shahrukh_Tcollection <- sum(shahrukh$Tcollection)
+    
+	akshay_Tcollection <- sum(akshay$Tcollection)
+    
+	amitabh_Tcollection <- sum(amitabh$Tcollection)
+
+  #sum of all the TCollection in the shahrukh, akshay and amitabh subsets respectively
+	
+#Q6  
+# Write command/s to find out how many movies are in Flop, Average, Hit and Superhit categories in the entire Bollywood data set.
+
+  summary(as.factor(bollywood$Verdict))
+  #converting the Verdict column to factor and using summary function to see the number of occurences of each Verdict factor in bollywood data frame
+  
+#You can use SAPPLY function if you want to apply a function specific columns in a data frame 
+#You can write a command to find the maximum value of Ocollection, Wcollection, Fwcollecion and Tcollection using sapply
+  
+#Q7 
+# Write a command to find the names of the movies which have the maximum Ocollection, Wcollection, Fwcollecion & Tcollection
+# Store the names of 4 movies in same sequence in movie_result vector
+
+ #get the index of the movie record with the highest value in collection category and using sapply to apply it to all collection category columns
+ 
+ movie_result<- bollywood$Movie[sapply(bollywood[,4:7],which.max)]
+	
+
+    
+ #end of solution   
+    
